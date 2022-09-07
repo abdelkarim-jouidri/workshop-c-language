552fc13b8308162ef30583564d76baab20aa3dfe
abdelkrim-jouidri <abdelkrimjouidri@gmail.com>
2022-09-07 14:52:19 +0100

    proposed code for the second challenge

--- week_01/challenge_2.c
+++ week_01/challenge_2.c
@@ -4,6 +4,6 @@ int main(void){
 	float temperature_C;
 	printf("enter a value of temperature in Celsius : ");
 	scanf("%f", &temperature_C);
-	temperature_F = (temperature_C - 32)* 1/1.8 ;
-	printf("your temperature degree in Celsius i : %f °F ", temperature_F);
+	temperature_F = (temperature_C*1.8)+32;
+	printf("your temperature in Fahrenheit is : %0.3f °F ", temperature_F);
 }
