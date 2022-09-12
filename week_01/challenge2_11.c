19a428e9cf8ee411222b3a307e2295cf819cee3c
abdelkarim-jouidri <112935195+abdelkarim-jouidri@users.noreply.github.com>
2022-09-12 09:32:48 +0100

    Delete challenge2_11.c

--- week_01/challenge2_11.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-int main(){
-    char *WEEK[] = {"monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"};
-    int integer;
-    int random = rand()%6;
-    printf("%s", WEEK[random] );
-
-}
